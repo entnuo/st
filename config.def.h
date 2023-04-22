c7c67e7a19ef6cfe269974d4466b3295bf2fd17b
entnuo <neptunemoon051@protonmail.com>
2023-04-22 04:53:49 -0300

    changed main font from liberation mono to hack nerd font

--- config.def.h
+++ config.def.h
@@ -5,12 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "monospace:pixelsize=13:antialias=true:autohint=true",
+    "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true",
     "Symbola:pixelsize=15:antialias=true:autohint=true",
-	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true",
+    "Liberation Mono:pixelsize=13:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
